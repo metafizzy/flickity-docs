a0cfde35fff694f08646072d79ad16ba81a03ea5
David DeSandro <desandrocodes@gmail.com>
2015-01-19 17:50:56 -0500

    try refactoring texta-fonts.js

--- js/texta-fonts.js
+++ js/texta-fonts.js
@@ -1,9 +1,11 @@
+/*jshint browser: true, undef: true, unused: true, evil: true */
+
 /*
 
  MyFonts Webfont Build ID 2956117, 2015-01-19T16:16:42-0500
 
  The fonts listed in this notice are subject to the End User License
- Agreement(s) entered into by the website owner. All other parties are 
+ Agreement(s) entered into by the website owner. All other parties are
  explicitly restricted from using the Licensed Webfonts(s).
 
  You may obtain a valid license at the URLs below.
@@ -29,27 +31,24 @@
 
 var customPath = '../fonts/';
 
-var protocol = document.location.protocol;
-"https:" != protocol && (protocol = "http:");
+// add MyFonts counter
 var count = document.createElement("script");
 count.type = "text/javascript";
-count.async = !0;
-count.src = protocol + "//hello.myfonts.net/count/2d1b55";
+count.async = true;
+count.src = "//hello.myfonts.net/count/2d1b55";
 var s = document.getElementsByTagName("script")[0];
 s.parentNode.insertBefore(count, s);
-var browserName, browserVersion, webfontType;
-if ("undefined" == typeof woffEnabled) var woffEnabled = !0;
-var svgEnabled = 1,
-  woff2Enabled = 1;
-if ("undefined" != typeof customPath) var path = customPath;
-else {
-  var scripts = document.getElementsByTagName("SCRIPT"),
-    script = scripts[scripts.length - 1].src;
-  script.match("://") || "/" == script.charAt(0) || (script = "./" + script);
-  path = script.replace(/\\/g, "/").replace(/\/[^\/]*\/?$/, "")
-}
-var wfpath = path,
-  browsers = [{
+
+var browserVersion, webfontType;
+
+var woffEnabled = true;
+var svgEnabled = true;
+var woff2Enabled = true;
+
+var path = customPath;
+var wfpath = path;
+
+var browsers = [{
     regex: "MSIE (\\d+\\.\\d+)",
     versionRegex: "new Number(RegExp.$1)",
     type: [{
@@ -105,7 +104,7 @@ var wfpath = path,
   }, {
     regex: "Mozilla.*(iPhone|iPad).* OS (\\d+)_(\\d+).* AppleWebKit.*Safari",
     versionRegex: "new Number(RegExp.$2) + (new Number(RegExp.$3) / 10)",
-    unhinted: !0,
+    unhinted: true,
     type: [{
       version: 5,
       type: "woff"
@@ -151,20 +150,47 @@ var wfpath = path,
   suffix = "",
   i = 0;
 a: for (; i < browLen; i++) {
-  var regex = RegExp(browsers[i].regex);
-  if (regex.test(navigator.userAgent)) {
-    browserVersion = eval(browsers[i].versionRegex);
-    var typeLen = browsers[i].type.length;
-    for (j = 0; j < typeLen; j++)
-      if (browserVersion >= browsers[i].type[j].version && (!0 == browsers[i].unhinted && (suffix = "_unhinted"), webfontType = browsers[i].type[j].type, "woff" != webfontType || woffEnabled) && ("woff2" != webfontType || woff2Enabled) && ("svg#wf" != webfontType || svgEnabled)) break a
-  } else webfontType = "woff"
+  var browser = browsers[i];
+  var regex = RegExp( browser.regex );
+
+  if ( regex.test( navigator.userAgent ) ) {
+    browserVersion = eval( browser.versionRegex );
+    var typeLen = browser.type.length;
+    for ( j = 0; j < typeLen; j++ ) {
+      var browserType = browser.type[j];
+
+      if ( browserVersion >= browserType.version ) {
+        suffix = browser.unhinted ? '_unhinted' : '';
+        webfontType = browserType.type;
+        var isWoffType = webfontType != 'woff' || woffEnabled;
+        var isWoff2Type = webfontType != 'woff2' || woff2Enabled;
+        var isSvgType = webfontType != 'svg#wf' || svgEnabled;
+
+        if ( isWoffType && isWoff2Type && isSvgType  ) {
+          break a;
+        }
+      }
+    }
+  } else {
+    webfontType = "woff";
+  }
 }
-/(Macintosh|Android)/.test(navigator.userAgent) && "svg#wf" != webfontType && (suffix = "_unhinted");
+
+
+
+
+// use unhinted on Mac & Android
+var isMacAndroid = /(Macintosh|Android)/.test( navigator.userAgent );
+if ( isMacAndroid && webfontType != 'svg#wf' ) {
+  suffix = "_unhinted";
+}
+
 var head = document.getElementsByTagName("head")[0],
   stylesheet = document.createElement("style");
 stylesheet.setAttribute("type", "text/css");
 head.appendChild(stylesheet);
-for (var fonts = [{
+
+var fonts = [{
     fontFamily: "Texta Heavy",
     url: wfpath + "2D1B55_0" + suffix + "_0." + webfontType
   }, {
@@ -173,14 +199,29 @@ for (var fonts = [{
   }, {
     fontFamily: "Texta Regular",
     url: wfpath + "2D1B55_2" + suffix + "_0." + webfontType
-  }], len = fonts.length, css = "", i = 0; i < len; i++) {
+  }];
+
+for ( var len = fonts.length, css = "", i = 0; i < len; i++ ) {
   var format = "svg#wf" == webfontType ? 'format("svg")' : "ttf" == webfontType ? 'format("truetype")' : "eot" == webfontType ? "" : 'format("' + webfontType + '")',
     css = css + ("@font-face{font-family: " + fonts[i].fontFamily + ";src:url(" + fonts[i].url + ")" + format +
       ";");
-  fonts[i].fontWeight && (css += "font-weight: " + fonts[i].fontWeight + ";");
-  fonts[i].fontStyle && (css += "font-style: " + fonts[i].fontStyle + ";");
-  css += "}"
+
+  if ( fonts[i].fontWeight ) {
+    css += "font-weight: " + fonts[i].fontWeight + ";";
+  }
+
+  if ( fonts[i].fontStyle ) {
+    css += "font-style: " + fonts[i].fontStyle + ";";
+  }
+
+  css += "}";
 }
-stylesheet.styleSheet ? stylesheet.styleSheet.cssText = css : stylesheet.innerHTML = css;
+
+
+if ( stylesheet.styleSheet ) {
+  stylesheet.styleSheet.cssText = css;
+} else {
+  stylesheet.innerHTML = css;
+}
 
 })();

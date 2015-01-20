bbf20dd0b920d6b1a119ef3987052224003bbb2c
David DeSandro <desandrocodes@gmail.com>
2015-01-20 12:42:48 -0500

    gulp js & css from bower gulp js gulp css gulp dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 /*jshint node: true, undef: true, unused: true */
 
 var gulp = require('gulp');
-
-var replace = require('gulp-replace');
-
-// --------------------------  -------------------------- //
-
+var concat = require('gulp-concat');
 var highlightjs = require('highlight.js');
+var replace = require('gulp-replace');
+var uglify = require('gulp-uglify');
+
+// ----- content ----- //
+
 highlightjs.configure({
   classPrefix: ''
 });
@@ -40,50 +41,89 @@ gulp.task( 'content', function() {
     .pipe( gulp.dest('build') );
 });
 
-// ----- copy assets ----- //
+// ----- prod assets ----- //
 
-var assetsSrc = [
-  'css/*.*',
-  'js/*.*'
+var prodAssetsSrc = [
+  'fonts/*.*'
 ];
 
-gulp.task( 'copy-assets', function() {
-  // base opt for copying directory content
-  // http://stackoverflow.com/a/25038015/182183
-  gulp.src( assetsSrc, { base: '.' } )
-    .pipe( gulp.dest('build') );
-});
-
-// ----- copy prod assets ----- //
-
-var prodAssetsSrc = assetsSrc.concat([
-  'fonts/*.*'
-]);
-
-gulp.task( 'copy-prod-assets', function() {
+// copy prod assets
+gulp.task( 'prod-assets', function() {
   gulp.src( prodAssetsSrc, { base: '.' } )
     .pipe( gulp.dest('build') );
 });
 
-// -----  ----- //
+// ----- dist ----- //
 
-// TODO copy flickity src
-// gulp.task( 'copy-flickity', function() {});
+// copy flickity dist to build/
+gulp.task( 'dist', function() {
+  gulp.src( 'bower_components/flickity/dist/*.*' )
+    .pipe( gulp.dest('build') );
+});
 
-// TODO concat css
+// ----- js ----- //
 
-// TODO concat & minify js
+var packageJsSrc = [
+  // dependencies
+  'bower_components/get-style-property/get-style-property.js',
+  'bower_components/get-size/get-size.js',
+  'bower_components/matches-selector/matches-selector.js',
+  'bower_components/eventEmitter/EventEmitter.js',
+  'bower_components/eventie/eventie.js',
+  'bower_components/doc-ready/doc-ready.js',
+  'bower_components/classie/classie.js',
+  'bower_components/imagesloaded/imagesloaded.js',
+  // flickity
+  'bower_components/flickity/js/utils.js',
+  'bower_components/flickity/js/unipointer.js',
+  'bower_components/flickity/js/cell.js',
+  'bower_components/flickity/js/prev-next-button.js',
+  'bower_components/flickity/js/page-dots.js',
+  'bower_components/flickity/js/player.js',
+  'bower_components/flickity/js/drag.js',
+  'bower_components/flickity/js/animate.js',
+  'bower_components/flickity/js/cell-change.js',
+  'bower_components/flickity/js/flickity.js'
+];
 
-// ----- watch ----- //
 
-gulp.task( 'watch', function() {
-  gulp.watch( contentSrc, [ 'content' ] );
-  gulp.watch( assetsSrc, [ 'copy-assets' ] );
+// concat & minify js
+gulp.task( 'js', function() {
+  gulp.src( packageJsSrc )
+    .pipe( uglify() )
+    .pipe( concat('flickity-docs.min.js') )
+    .pipe( gulp.dest('build/js') );
+});
+
+
+// ----- css ----- //
+
+var cssSrc = [
+  // flickity
+  'bower_components/flickity/css/flickity.css',
+  // docs
+  'css/styles.css'
+];
+
+gulp.task( 'css', function() {
+  gulp.src( cssSrc )
+    .pipe( concat('flickity-docs.css') )
+    .pipe( gulp.dest('build/css') );
 });
 
 // ----- default ----- //
 
 gulp.task( 'default', [
   'content',
-  'copy-prod-assets'
+  'js',
+  'css',
+  'dist',
+  'prod-assets'
 ] );
+
+// ----- watch ----- //
+
+gulp.task( 'watch', function() {
+  gulp.watch( contentSrc, [ 'content' ] );
+  gulp.watch( 'css/*.css', [ 'css' ] );
+});
--- notes.md
+++ notes.md
@@ -7,9 +7,9 @@
 <!-- mobile hero gallery -->
 <!-- GitHub link -->
 <!-- copy assets to build/ -->
-copy flickity files to build/
+<!-- copy flickity files to build/ -->
 dev build
   don't copy fonts
   list out all sources
 <!-- figure out s3cmd -->
-tag version 0.1.0, npm publish
+<!-- tag version 0.1.0, npm publish -->

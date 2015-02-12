836016130df11a9476cd5bffe9f3b44fa23f2c8b
David DeSandro <desandrocodes@gmail.com>
2015-02-12 17:27:35 -0500

    positionCells -> reposition

---
+++ js/modules/reposition.js
@@ -0,0 +1,15 @@
+FlickityDocs.modules.reposition = function( elem ) {
+  'use strict';
+
+  var gallery = elem.querySelector('.gallery');
+  var flkty = new Flickity( gallery );
+
+  flkty.on( 'staticClick', function( event, pointer, cellIndex, cellElement ) {
+    if ( !cellElement ) {
+      return;
+    }
+    classie.toggle( cellElement, 'is-expanded' );
+    flkty.reposition();
+  });
+
+};

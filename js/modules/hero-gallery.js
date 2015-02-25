f01322ddec44bd715c4e305dc3daffe369d5b5c8
David DeSandro <desandrocodes@gmail.com>
2015-02-25 15:33:53 -0500

    add hero video with js

--- js/modules/hero-gallery.js
+++ js/modules/hero-gallery.js
@@ -1,6 +1,25 @@
 FlickityDocs.modules['hero-gallery'] = function( elem ) {
   'use strict';
 
-  
+  // add video
+  ( function() {
+    var video = document.createElement('video');
+    // stop if video not supported
+    if ( !video || !video.canPlayType ||
+      !video.canPlayType('video/mp4; codecs="avc1.42E01E"') ) {
+        return;
+    }
+
+    video.setAttribute( 'loop', 'loop' );
+    video.className = 'hero-illustration';
+    video.src = 'img/flickity-illustration.mp4';
+
+    // add video to page when loaded
+    video.addEventListener( 'loadeddata', function() {
+      var cellContent = elem.querySelector('.hero-gallery__cell--1 .hero-gallery__cell__content');
+      video.play();
+      cellContent.appendChild( video );
+    }, false );
+  })();
 
 };

d07f9433242fe799a95069564721c5808679b53f
David DeSandro <desandrocodes@gmail.com>
2015-02-26 08:32:46 -0500

    no illustration in export

--- js/modules/hero-gallery.js
+++ js/modules/hero-gallery.js
@@ -2,12 +2,15 @@ FlickityDocs.modules['hero-gallery'] = function( elem ) {
   'use strict';
 
   var firstCell = elem.querySelector('.hero-gallery__cell--1');
-  var illoImg = firstCell.querySelector('img');
-  // switch out PNG for GIF
-  var proxyGif = document.createElement('img');
-  eventie.bind( proxyGif, 'load', function() {
-    illoImg.src = proxyGif.src;
-  });
-  proxyGif.src = 'img/flickity-illustration.gif';
+  var illoImg = firstCell.querySelector('.hero-illustration');
+
+  if ( illoImg ) {
+    // switch out PNG for GIF
+    var proxyGif = document.createElement('img');
+    eventie.bind( proxyGif, 'load', function() {
+      illoImg.src = proxyGif.src;
+    });
+    proxyGif.src = 'img/flickity-illustration.gif';
+  }
 
 };

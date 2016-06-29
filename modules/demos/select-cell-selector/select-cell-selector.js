96263bb54a9f0235b900ec2c0307d97ccd669823
David DeSandro <desandrocodes@gmail.com>
2016-06-29 09:17:27 -0400

    📝 selectCell method

---
+++ modules/demos/select-cell-selector/select-cell-selector.js
@@ -0,0 +1,15 @@
+FlickityDocs.modules['select-cell-selector'] = function( elem ) {
+  'use strict';
+
+  var flkty = new Flickity( elem.querySelector('.carousel'), {
+    groupCells: true,
+  });
+
+  var buttonGroup = elem.querySelector('.button-group');
+
+  filterBind( buttonGroup, 'click', '.button', function( event ) {
+    var selector = event.target.getAttribute('data-selector');
+    flkty.selectCell( selector );
+  });
+
+};
---
+++ modules/demos/select-cell/select-cell.js
@@ -0,0 +1,18 @@
+FlickityDocs.modules['select-cell'] = function( elem ) {
+  'use strict';
+
+  var utils = window.fizzyUIUtils;
+
+  var flkty = new Flickity( elem.querySelector('.carousel'), {
+    groupCells: true,
+  });
+
+  var buttonGroup = elem.querySelector('.button-group');
+  var buttons = utils.makeArray( buttonGroup.querySelectorAll('.button') );
+
+  filterBind( buttonGroup, 'click', '.button', function( event ) {
+    var index = buttons.indexOf( event.target );
+    flkty.selectCell( index );
+  });
+
+};
